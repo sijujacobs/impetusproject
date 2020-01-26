a3ca8e60b6b5de45318d54dac943ee352b2a4e0c
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-26 14:37:30 -0500

    homepage changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,7 @@ import React from 'react';
 
 const Home = (props) => {
     return(<div className="home">
-           {/* <div class="backdrop">
-                <p class="text multiply">REACT JS 1</p>
-            </div>
-            <div class="backdrop">
-                <p class="text screen">REACT JS 2</p>
-            </div>
-            <div class="backdrop">
-                <p class="text darken">REACT JS 3</p>
-            </div> */}
-            {/* <div class="backdrop">
-                <p class="text lighten">REACT JS 4</p>
-                
-            </div> */}
-<p class="text2">REACT JS 4</p>
+           HOME
         </div>)
 }
 
