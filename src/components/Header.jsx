ca00c8d7d3ee30f99aa819b2c5fbb7cd1af07861
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-26 07:58:34 -0500

    CSS changes for ProductList and Cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+
+import {connect} from 'react-redux';
 const Header = (props) => {
 
     return (<div className="header">
-        <ul className="navMenu">
-            <li><NavLink exact to="/">Homes</NavLink></li>
-            <li><NavLink exact to="/cartProject">Project</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
+        <div className="menuBar">
+            <ul className="navMenu">
+                <li><NavLink exact to="/">Homes</NavLink></li>
+                <li><NavLink exact to="/cartProject">Project</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+        </div>
         <div className="statusSection">
-            <span className="cartStatus"></span>
+            <span className="cartStatus">{props.cartItems.length + " : Items"}</span>
         </div>
     </div>)
 }
+const mapStateToProps = (state) => {
+    return {
+        cartItems : state.cartReducer.cartItems
+    }
+}
 
-export default Header;
+const connectedHeader = connect(mapStateToProps)(Header);
+export default connectedHeader;
