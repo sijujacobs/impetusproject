8a412fa8564f89a699e024755ad185bd711c18ee
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-26 10:54:14 -0500

    Updates

--- precache-manifest.7211101128a73cf3017e94fe8902dcea.js
+++ precache-manifest.7211101128a73cf3017e94fe8902dcea.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e454b949f46d181435e57bea15ee7f76",
+    "revision": "11cc7bedf4c914437219caf3efc56e3f",
     "url": "/impetusproject/index.html"
   },
   {
-    "revision": "3773edf470fda7f27436",
-    "url": "/impetusproject/static/css/main.bb7ce3b0.chunk.css"
+    "revision": "0d8f1fc08f96bbbbc710",
+    "url": "/impetusproject/static/css/main.8298acdb.chunk.css"
   },
   {
     "revision": "83b2d1d850bb3d84e411",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/impetusproject/static/js/2.52203d9e.chunk.js.LICENSE"
   },
   {
-    "revision": "3773edf470fda7f27436",
+    "revision": "0d8f1fc08f96bbbbc710",
     "url": "/impetusproject/static/js/main.3f4e4e63.chunk.js"
   },
   {
