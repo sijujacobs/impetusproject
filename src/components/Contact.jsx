a587c32ae648f66008891116ed62e3618c955e9f
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-27 07:49:59 -0500

    Updated documentation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
 
 const Contact = (props) => {
-    return(<div className="contact">
-           Documentation
-        </div>)
+    return (<div className="documentation">
+        <div className="documentHeader">About Project</div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                Tools & Technologies
+            </div>
+            <div className="topicDesc">
+                <p>ReactJS</p>
+                <p>Redux, React-Redux, React-Thunk, Axios , gh-pages</p>
+                <p>HTML, CSS, SASS</p>
+                <p>jest</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                React App
+            </div>
+            <div className="topicDesc">
+                <p>SPA(Single Page Application) with React-router</p>
+                <p>3 Views - Home, Project and Docs</p>
+                <p>Project - ShoppingCart App</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                API 
+            </div>
+            <div className="topicDesc">
+                <a alt="jsonLink" href="https://jsonplaceholder.typicode.com">JSOMPlaceHolder</a>
+                <p>https://jsonplaceholder.typicode.com/</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                <p>Photos API </p>
+            </div>
+            <div className="topicDesc">
+            <a alt="jsonLink" href="https://jsonplaceholder.typicode.com/photos">PHOTOS</a>
+            <p>https://jsonplaceholder.typicode.com/photos</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                <p>Repository </p>
+            </div>
+            <div className="topicDesc">
+            <a alt="git" href="https://github.com">GITHUB</a>
+            <p>https://github.com</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                <p>Live Demo</p>
+            </div>
+            <div className="topicDesc">
+            <a alt="git" href="https://sijujacobs.github.io/impetusproject/">Demo</a>
+            <p>https://sijujacobs.github.io/impetusproject/</p>
+            </div>
+        </div>
+        <div className="topicRow">
+            <div className="topicLabel">
+                <p>Source Code </p>
+            </div>
+            <div className="topicDesc">
+            <a alt="git" href="https://github.com/sijujacobs/impetusproject">Project</a>
+            <p>https://github.com/sijujacobs/impetusproject</p>
+            </div>
+        </div>
+    </div>)
 }
 
 export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Home = (props) => {
     return(<div className="home">
-           <p class="text2">REACT APP</p>
+           <p className="text2">REACT APP</p>
+           <div className="homeDesc">
+            <p>A project built on ReactJS.</p>
+            <p>This project explores REDUX, REACT-THUNK and AXIOS </p>
+            <p>Shopping Cart App is for demo pusrpose only</p>
+           </div>
         </div>)
 }
 
